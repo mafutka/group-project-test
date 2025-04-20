61d624149767bc549fdbe652444baf4e82668e64
mafutka <mafutka@gmail.com>
2025-04-20 12:56:46 +0300

    categories_tovariv

--- src/home.js
+++ src/home.js
@@ -1,1 +1,23 @@
 //Логіка сторінки Home
+import { fetchCategories } from "./js/products-api";
+
+const categoriesList = document.querySelector(".categories");
+
+fetchCategories()
+.then(categories => {
+    const allCategories = ["All", ...categories];
+        renderCategories(allCategories);
+    })
+    .catch(error => {
+        console.log("No categories to get", error);
+        
+    });
+
+    function renderCategories(categories) {
+        const markup = categories.map(category => `
+            <button class="categories__btn" type="button">${category}</button>
+    </li>
+            `).join("");
+
+            categoriesList.innerHTML = markup;
+    }
--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,14 @@
 // Функції для роботи з бекендом
+export function fetchCategories() {
+    return fetch('https://dummyjson.com/products/categories')
+        .then(res => {
+            if(!res.ok) {
+                throw new Error(res.stastusText);
+            }
+            return res.json();
+        })
+}
+
+export function fetchProducts() {
+    
+}
